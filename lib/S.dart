e38f472345a6de7bc40755bb31c323d8d32571c2
RAKIB <rakibulislam10.net@gmail.com>
2023-12-29 21:29:45 +0600

    on page to another page

--- lib/S.dart
+++ lib/S.dart
@@ -1,20 +1,35 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
+  var data;
+  SecondScreen(this.data);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.pink,
-        title: Text(
-          " Wellcome To New Page",
-          style: TextStyle(fontSize: 20),
-        ),
+      appBar: AppBar(title: Text("Data Passing"),),
+      drawer: Drawer(),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home",),
+        BottomNavigationBarItem(icon: Icon(Icons.email),label: "Message",),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Profile",),
+
+      ]),
+
+      body: Column(
+        children: [
+          SizedBox(width: 360,),
+          Padding(
+            padding: const EdgeInsets.only(top: 200.0),
+            child: Text("👋 Hi, $data",
+              style: TextStyle(fontSize: 32,
+                  fontWeight: FontWeight.bold),),
+          )
+        ],
       ),
-      body: Center(
-          child: Text(
-            "Wellcome Rakib",
-            style: TextStyle(fontSize: 40),
-          )),
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'S.dart';
 
-void main(){
+void main() {
   runApp(RakibApp());
 }
 
@@ -10,22 +11,71 @@ class RakibApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Colors.pink,
+          )),
+      title: "Rakib App",
       debugShowCheckedModeBanner: false,
-      title: "Rakib",
       home: FristScreen(),
     );
   }
 }
 
 class FristScreen extends StatelessWidget {
+  var NameControler = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(),
-        body: Center(child: ElevatedButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen(),));
-        }, child: Text("Next"),))
+      body: Center(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 120.0),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 19.0),
+                child: Text(
+                  "Enter your name for next page",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                controller: NameControler ,
+                decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.drive_file_rename_outline),
+                    hintText: "Please enter your name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 98.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondScreen(NameControler.text.toString()),
+                      ));
+                },
+                child: Text("Next"),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    minimumSize: Size(10, 50),
+                    textStyle:
+                    TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
