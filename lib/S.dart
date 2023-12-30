6f8da6be1ec89bb9f7f0735990f4072ddcecbc11
RAKIB <rakibulislam10.net@gmail.com>
2023-12-30 16:22:35 +0600

    user input ifo singup from

---
+++ lib/S.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    minimumSize: Size(double.infinity, 55)
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(RakibAlert());
+import 'S.dart';
+
+void main(){
+  runApp(User());
 }
 
-class RakibAlert extends StatelessWidget {
+class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
+return MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomeScreen(),
+);
   }
 }
 
-class Home extends StatelessWidget {
-  MySnacbar(msg, context) {
-    return ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(msg)));
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Rakib"),
-      ),
-      drawer: Drawer(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("Are you Sure?"),
-                    content: Text("Do you want to delete?"),
-                    actions: [
-                      TextButton(
-                          onPressed: () {
-                            MySnacbar("Delete Succesfull", context);
-                            Navigator.pop(context);
-                          },
-                          child: Text("Yes")),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text("No")),
-                    ],
-                  );
-                });
-          },
-          child: Text("Delete"),
-        ),
-      ),
-    );
+ return Scaffold(
+   body: Column(
+     mainAxisAlignment: MainAxisAlignment.start,
+     children: [
+       Padding(padding: EdgeInsets.all(10),child:  TextField(decoration: InputDecoration(label: Text("Name"),border: OutlineInputBorder()),),),
+       Padding(padding: EdgeInsets.all(10),child:  TextField(decoration: InputDecoration(label: Text("Email"),border: OutlineInputBorder()),),),
+       Padding(padding: EdgeInsets.all(10),child:  TextField(decoration: InputDecoration(label: Text("Phone"),border: OutlineInputBorder()),),),
+       Padding(padding: EdgeInsets.all(10),child: ElevatedButton(onPressed: (){},child: Text("Submit"),style: buttonStyle,)),
+
+     ],
+   ),
+ );
   }
 }
