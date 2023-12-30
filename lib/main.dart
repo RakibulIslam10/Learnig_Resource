0be486fca14098edb6f256d640f963f380ba28e6
RAKIB <rakibulislam10.net@gmail.com>
2023-12-30 14:05:48 +0600

    new alert box

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,58 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'S.dart';
-
 void main() {
-  runApp(RakibApp());
+  runApp(RakibAlert());
 }
 
-class RakibApp extends StatelessWidget {
+class RakibAlert extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(
-            color: Colors.pink,
-          )),
-      title: "Rakib App",
       debugShowCheckedModeBanner: false,
-      home: FristScreen(),
+      home: Home(),
     );
   }
 }
 
-class FristScreen extends StatelessWidget {
-  var NameControler = TextEditingController();
+class Home extends StatelessWidget {
+  MySnacbar(msg, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(msg)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Rakib"),
+      ),
+      drawer: Drawer(),
       body: Center(
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 120.0),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 19.0),
-                child: Text(
-                  "Enter your name for next page",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: TextField(
-                controller: NameControler ,
-                decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.drive_file_rename_outline),
-                    hintText: "Please enter your name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    )),
-              ),
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 98.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SecondScreen(NameControler.text.toString()),
-                      ));
-                },
-                child: Text("Next"),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    minimumSize: Size(10, 50),
-                    textStyle:
-                    TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
-              ),
-            )
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Are you Sure?"),
+                    content: Text("Do you want to delete?"),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            MySnacbar("Delete Succesfull", context);
+                            Navigator.pop(context);
+                          },
+                          child: Text("Yes")),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("No")),
+                    ],
+                  );
+                });
+          },
+          child: Text("Delete"),
         ),
       ),
     );
