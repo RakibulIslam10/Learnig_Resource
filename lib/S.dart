66ec2c1601bf35f65b23c1598ea2921403b3f728
RAKIB <rakibulislam10.net@gmail.com>
2024-01-04 15:24:22 +0600

    with login ui

--- lib/S.dart
+++ lib/S.dart
@@ -1,5 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-    minimumSize: Size(double.infinity, 55)
-);
+import 'ProfilePage.dart';
+import 'tt.dart';
+
+class MyBottomBar extends StatelessWidget {
+  const MyBottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (value) {
+          if (value == 1) {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const NotificationPage()));}
+          if(value == 2){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => MyProfilePage(),));}
+        },
+        iconSize: 28,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.bell), label: "Notification"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person), label: "Home"),
+        ]);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,32 @@ import 'package:flutter/material.dart';
 import 'S.dart';
 
 void main(){
-  runApp(User());
+  runApp(const UserProfile());
 }
+class UserProfile extends StatelessWidget {
+  const UserProfile({super.key});
 
-class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-return MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: HomeScreen(),
-);
+    return const MaterialApp(
+      title: "User Profile",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-class HomeScreen extends StatelessWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
- return Scaffold(
-   body: Column(
-     mainAxisAlignment: MainAxisAlignment.start,
-     children: [
-       Padding(padding: EdgeInsets.all(10),child:  TextField(decoration: InputDecoration(label: Text("Name"),border: OutlineInputBorder()),),),
-       Padding(padding: EdgeInsets.all(10),child:  TextField(decoration: InputDecoration(label: Text("Email"),border: OutlineInputBorder()),),),
-       Padding(padding: EdgeInsets.all(10),child:  TextField(decoration: InputDecoration(label: Text("Phone"),border: OutlineInputBorder()),),),
-       Padding(padding: EdgeInsets.all(10),child: ElevatedButton(onPressed: (){},child: Text("Submit"),style: buttonStyle,)),
-
-     ],
-   ),
- );
+    return Scaffold(
+      bottomNavigationBar: MyBottomBar(),
+    );
   }
 }
